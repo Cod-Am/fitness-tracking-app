51602276412a2bacc88650ef043b1ad86192dc26
Cod-Am <amrit.672004@gmail.com>
2023-07-14 23:46:49 +0530

    done for now

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
 import Nav from './components/Nav/Nav';
 import Banner from './components/Banner/Banner';
-import Options from './components/Options/Options';
+
 function App() {
   return (
     <div className="App">
-      {/* Nav Bar */}
-      <Nav />
-      {/* Banner */}
-      <Banner />
-      {/* Options */}
-      {/* <Options /> */}
+      <div className='firstpage'>{/* Nav Bar */}
+        <Nav />
+        {/* Banner */}
+        <Banner />
+      </div>
     </div>
   );
 }
